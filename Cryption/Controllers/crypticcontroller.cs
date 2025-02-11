e70bc87518ee7b88b5b6f662c29a53d4103ec47c
Ömer Yavuz <omeryavuz0927@gmail.com>
2025-02-11 12:23:35 +0100

    added controller for endpoints with logic

---
+++ Cryption/Controllers/crypticcontroller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+using Cryption.services;
+
+namespace Cryption.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class crypticcontroller : ControllerBase
+    {
+        private const int Shift = 3; 
+
+        [HttpPost("encrypt")]
+        public IActionResult Encrypt([FromBody] string plainText)
+        {
+            try
+            {
+                string encryptedText = crypticservices.Encrypt(plainText, Shift);
+                return Ok(new { EncryptedText = encryptedText });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+
+        [HttpPost("decrypt")]
+        public IActionResult Decrypt([FromBody] string cipherText)
+        {
+            try
+            {
+                string decryptedText = crypticservices.Decrypt(cipherText, Shift);
+                return Ok(new { DecryptedText = decryptedText });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+
+    }
+}
